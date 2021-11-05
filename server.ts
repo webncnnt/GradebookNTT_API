f8f39e2b613d34b4c780145d8d354c8edeb0cb97
Minh Tri <ndmt1at21@DESKTOP-SG1REEV.localdomain>
2021-11-05 07:45:47 +0700

    config test cicd

--- server.ts
+++ server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.use('*', (req, res, next) => {
+app.use('/api/dashboard', (req, res, next) => {
 	res.send('<h1>Hello from node</h1>');
+});
+
+app.use('/api/users', (req, res, next) => {
+	res.send('<h1>All users</h1>');
 });
 
 app.listen(8000, () => {
