49ac635e27436f6e01f5f61d90cc1a2f0a7d7799
huynhthinhi <huynhthinhi206@gmail.com>
2021-11-07 21:29:37 +0700

    create table User

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const db = require("../db.js");
+const sequelize = require("sequelize");
+
+
+const User = db.define("User", {
+
+  fullname: sequelize.STRING,
+  email: sequelize.STRING,
+  password: sequelize.STRING,
+  role: sequelize.INTEGER,
+  studentId: sequelize.STRING,
+  avatar: sequelize.STRING,
+  status: sequelize.BOOLEAN // true: block
+});
+
+db.sync()
+.then(()=>{
+    console.log("Create user successfully...")
+})
+
+module.exports = User
