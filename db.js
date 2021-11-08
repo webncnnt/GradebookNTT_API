a51a1d77b217764681591c8e0d4a2f8d5ec0c204
ndmt1at21 <trminh36@gmail.com>
2021-11-08 22:04:41 +0700

    config db connection infor

--- db.js
+++ db.js
@@ -1,30 +1,26 @@
-const Sequelize = require('sequelize')
-const dotenv = require('dotenv');
-
-dotenv.config();
-
+const { config } = require('config');
+const Sequelize = require('sequelize');
 
 const database = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+	database: config.DB_NAME,
+	username: config.DB_USERNAME,
+	password: config.DB_PASSWORD,
 
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    
-    // dialectOptions: {
-    //     ssl: {
-    //       require: true,
-    //       rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-    //     }
-    //   },
+	host: config.DB_HOST,
+	port: config.DB_PORT,
+	dialect: 'postgres'
 
-	
-})
+	// dialectOptions: {
+	//     ssl: {
+	//       require: true,
+	//       rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+	//     }
+	//   },
+});
 
-database.authenticate()
-.then(() => console.log('Connect database successfully'))
-.catch((e)=> console.log(e))
+database
+	.authenticate()
+	.then(() => console.log('Connect database successfully'))
+	.catch(e => console.log(e));
 
-module.exports = database
+module.exports = database;
