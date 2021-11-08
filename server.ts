73355bd986a3ed48d638be1681a1decbf3df3fec
ndmt1at21 <trminh36@gmail.com>
2021-11-08 09:34:46 +0700

    change server

--- server.ts
+++ server.ts
@@ -6,10 +6,6 @@ app.use('/api/dashboard', (req, res, next) => {
 	res.send('<h1>Hello from node</h1>');
 });
 
-app.use('/api/users', (req, res, next) => {
-	res.send('<h1>All users</h1>');
-});
-
 app.listen(8000, () => {
 	console.log('Server is listening on port 8000');
 });
