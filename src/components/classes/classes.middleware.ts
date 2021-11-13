fe8f44e3ad0ad2c77bd358047e1eda2e4f3b6012
ndmt1at21 <trminh36@gmail.com>
2021-11-13 10:13:25 +0700

    add classes middleware to check if id param is exist

---
+++ src/components/classes/classes.middleware.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from 'express';
+import { AppError } from '@src/utils/appError';
+import { ClassesMessageError } from './classes.constant';
+import { HttpStatusCode } from '@src/constant/httpStatusCode';
+
+export const validateIdParamsExist: RequestHandler = (req, res, next) => {
+	const id = +req.params.id;
+
+	if (!id)
+		throw new AppError(
+			HttpStatusCode.BAD_REQUEST,
+			ClassesMessageError.INVALID_CLASS_ID
+		);
+
+	next();
+};
