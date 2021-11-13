8b652ca96256d00674c64aceb5eaef25176d499b
ndmt1at21 <trminh36@gmail.com>
2021-11-12 17:04:02 -0800

    add constant: httpcode, common message error

---
+++ src/constant/authorizeError.ts
@@ -0,0 +1,4 @@
+export class AuthorizeMessageError {
+	static readonly UNAUTHORIZED =
+		'User is not authorized to perform the desired action';
+}
---
+++ src/constant/httpStatusCode.ts
@@ -0,0 +1,7 @@
+export class HttpStatusCode {
+	static readonly OK = 200;
+	static readonly BAD_REQUEST = 400;
+	static readonly INTERNAL_SERVER_ERROR = 500;
+	static readonly UNAUTHORIZED = 401;
+	static readonly NOT_FOUND = 404;
+}
