e56234734ccf83213d5a54bae9bac059a3a7b94e
ndmt1at21 <trminh36@gmail.com>
2021-11-20 22:06:14 +0700

    add class invitations components

---
+++ src/components/classInvitation/index.ts
@@ -0,0 +1,8 @@
+import { isAuth } from '@src/auth/auth.middleware';
+import { Router } from 'express';
+import classInvitationRouter from './classInvitations.api';
+
+const router = Router();
+router.use('/', isAuth, classInvitationRouter);
+
+export default router;
