e54dd6a18fd9435fee4935ba88f10719403cebde
ndmt1at21 <trminh36@gmail.com>
2021-11-20 20:59:43 +0700

    add attributes  interface for user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,56 @@
 import sequelize from '@src/db/sequelize';
-import { DataTypes } from 'sequelize';
+import {
+	DataTypes,
+	HasManyAddAssociationMixin,
+	HasManyCountAssociationsMixin,
+	HasManyGetAssociationsMixin,
+	HasManyRemoveAssociationMixin,
+	Model
+} from 'sequelize';
+import { Class } from './Class';
 
-export const User = sequelize.define('User', {
-	fullname: DataTypes.STRING,
-	email: DataTypes.STRING,
-	password: DataTypes.STRING,
-	role: DataTypes.INTEGER,
-	studentId: DataTypes.STRING,
-	avatar: DataTypes.STRING,
-	status: DataTypes.BOOLEAN,// true: block
-	dob: DataTypes.DATE,
-	address: DataTypes.STRING,
-	numberPhone: DataTypes.STRING,
-	facebook: DataTypes.STRING
-});
+export class User extends Model {
+	id!: number;
+	fullname!: string;
+	email!: string;
+	password!: string;
+	role!: number;
+	studentId!: number;
+	avatar!: string | null;
+	status!: boolean; // true: block
+	dob!: Date | null;
+	address!: string | null;
+	numberPhone!: string | null;
+	facebook!: string | null;
+
+	static async findUserByEmail(email: string): Promise<User | null> {
+		return await User.findOne({ where: { email } });
+	}
+
+	public countClasses!: HasManyCountAssociationsMixin;
+	public getClasses!: HasManyGetAssociationsMixin<Class>;
+	public addClass!: HasManyAddAssociationMixin<Class, number>;
+	public removeClass!: HasManyRemoveAssociationMixin<Class, number>;
+}
+
+User.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		fullname: DataTypes.STRING,
+		email: DataTypes.STRING,
+		password: DataTypes.STRING,
+		role: DataTypes.INTEGER,
+		studentId: DataTypes.STRING,
+		avatar: DataTypes.STRING,
+		status: DataTypes.BOOLEAN, // true: block
+		dob: DataTypes.DATE,
+		address: DataTypes.STRING,
+		numberPhone: DataTypes.STRING,
+		facebook: DataTypes.STRING
+	},
+	{ sequelize, modelName: 'Users' }
+);
