fcecd6a9e05aa8439f31a29a42316d7744ede941
ndmt1at21 <trminh36@gmail.com>
2021-11-20 22:23:19 +0700

    app apperror

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,15 +1,31 @@
 export class AppError extends Error {
 	isOperational: boolean;
-	statusCode: number;
 	message: string;
 
-	constructor(statusCode: number, message: string) {
+	constructor(message: string) {
 		super();
 
 		this.isOperational = true;
-		this.statusCode = statusCode;
 		this.message = message;
 
 		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export class NotFoundError extends AppError {
+	constructor(message: string) {
+		super(message);
+	}
+}
+
+export class IllegalArgumentError extends AppError {
+	constructor(message: string) {
+		super(message);
+	}
+}
+
+export class UnauthorizedError extends AppError {
+	constructor(message: string) {
+		super(message);
 	}
 }
