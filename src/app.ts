1449612df4184542883a034e5c6d750ec4651cbb
ndmt1at21 <trminh36@gmail.com>
2021-11-23 21:11:39 +0700

    add static folder

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+app.use(express.static('/public'));
+
 app.use(
 	cors({
 		origin: ['http://localhost:3000', 'http://locahost:8000'],
