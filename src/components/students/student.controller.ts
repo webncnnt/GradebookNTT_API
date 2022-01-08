2b1edeae31ee7764fced876dbff8ec86fb1a3975
ndmt1at21 <trminh36@gmail.com>
2022-01-08 15:34:15 +0700

    get student grades in class

--- src/components/students/student.controller.ts
+++ src/components/students/student.controller.ts
@@ -1,10 +1,22 @@
-import { deleteDataStudent, findStudentsByClassId, saveStudent, findStudentByStudentId } from './student.model';
+import {
+	deleteDataStudent,
+	findStudentsByClassId,
+	saveStudent,
+	findStudentByStudentId
+} from './student.model';
 import { findUserIdByStudentId } from '@src/auth/users.model';
-import { saveStudentgrade, updateGradeStudent } from './grade.model';
-import { findAssignmentById, findClassNameByClassId, findEmailStudentByClassId } from './student.model';
+import {
+	findAllGradeStudentByClassId,
+	saveStudentgrade,
+	updateGradeStudent
+} from './grade.model';
+import {
+	findAssignmentById,
+	findClassNameByClassId,
+	findEmailStudentByClassId
+} from './student.model';
 import { sendEmail } from '../mailServices/mail.service';
 import { config } from '@src/config';
-
 
 export interface IHash {
 	[details: number]: string;
@@ -23,59 +35,69 @@ export const uploadStudent = async (students: any, classId: number) => {
 			students[i].studentName,
 			userId,
 			classId
-
 		);
 	}
-    return hashMap;
+	return hashMap;
 };
 
-export const inputGrade = async(studentId: string, score: number, assignmentId: number)=>{
-
+export const inputGrade = async (
+	studentId: string,
+	score: number,
+	assignmentId: number
+) => {
 	const assign = await findAssignmentById(assignmentId);
 	const student = await findStudentByStudentId(studentId);
 
-	if(assign == null || student == null)
-		return false;
+	if (assign == null || student == null) return false;
 	await saveStudentgrade(studentId, score, assignmentId);
 	return true;
-}
+};
 
-export const getStudentsByClassId = async(classId: number) =>{
-	const result = await findStudentsByClassId(classId)
+export const getStudentsByClassId = async (classId: number) => {
+	const result = await findStudentsByClassId(classId);
 	return result;
-}
+};
 
-export const markFinalizeGrade = async (assignmentId: number) =>{
-	 //find class id ->name class -> asigmnent name -> tim student -> email
-	 const assignment: any = await findAssignmentById(assignmentId);
-	 const className = await findClassNameByClassId(assignment.classId);
-	 const title = assignment.title;
-	 const emailList: any = await findEmailStudentByClassId(assignment.classId);
-	
-	 const msg = {
+export const markFinalizeGrade = async (assignmentId: number) => {
+	//find class id ->name class -> asigmnent name -> tim student -> email
+	const assignment: any = await findAssignmentById(assignmentId);
+	const className = await findClassNameByClassId(assignment.classId);
+	const title = assignment.title;
+	const emailList: any = await findEmailStudentByClassId(assignment.classId);
+
+	const msg = {
 		to: emailList,
-		from: { email: 'classroom@gradebook.codes'},
+		from: { email: 'classroom@gradebook.codes' },
 		subject: `Grade for assignment ${title} of ${className} class`,
 		html: `<h3>Got marks for assignment ${title} of ${className} class</h3> <a href = '${config.DOMAIN}/class-detail/${assignment.classId}/scores'>
 		Click here to view scores</a>`
 	};
 
-	if(emailList.length > 0){
+	if (emailList.length > 0) {
 		let result = await sendEmail(msg);
-	 	return result;
+		return result;
+	} else {
+		console.log('Email list is empty!!');
 	}
-	else{
-		console.log("Email list is empty!!");
-	
-	}
-}
+};
 
-export const updateGrade = async(studentId: string, score: number, assignmentId: number)=>{
+export const updateGrade = async (
+	studentId: string,
+	score: number,
+	assignmentId: number
+) => {
 	const assign = await findAssignmentById(assignmentId);
 	const student = await findStudentByStudentId(studentId);
 
-	if(assign == null || student == null)
-		return false;
+	if (assign == null || student == null) return false;
 	await updateGradeStudent(studentId, score, assignmentId);
 	return true;
-}
+};
+
+export const getAllStudentGradeInClass = async (
+	studentId: string,
+	classId: number
+) => {
+	const grades = await findAllGradeStudentByClassId(classId, studentId);
+	return grades;
+};
--- src/components/students/student.model.ts
+++ src/components/students/student.model.ts
@@ -1,78 +1,76 @@
-
-import { Class } from "@src/models/Class";
-import { GradeAssignment } from "@src/models/GradeAssignment";
-import { Student } from "@src/models/Student";
+import { Class } from '@src/models/Class';
+import { GradeAssignment } from '@src/models/GradeAssignment';
+import { Student } from '@src/models/Student';
 import database from '@src/db';
 
+export const saveStudent = async (
+	studentId: string,
+	studentName: string,
+	userId: number,
+	classId: number
+) => {
+	Student.create({
+		fullName: studentName,
+		studentId: studentId,
+		userId: userId,
+		classId: classId
+	});
+};
 
+export const findStudentsByClassId = async (classId: number) => {
+	const result = await Student.findAll({
+		where: {
+			classId: classId
+		}
+	});
 
-export const saveStudent = async (studentId: string, studentName:string, userId: number, classId: number) =>{
+	return result;
+};
 
-   Student.create({
-       fullName: studentName,
-       studentId: studentId,
-       userId: userId,
-       classId: classId
-   });
-}
+export const findAssignmentById = async (id: number) => {
+	const result = await GradeAssignment.findOne({
+		where: {
+			id: id
+		}
+	});
 
-export const findStudentsByClassId = async (classId: number) =>{
-    const result = await Student.findAll({
-        where:{
-            classId: classId,
-        }
-    })
+	return result;
+};
 
-    return result;
-}
-
-export const findAssignmentById = async (id: number) =>{
-    const result = await GradeAssignment.findOne({
-        where:{
-            id: id
-        }
-    })
-
-    return result;
-}
-
-export const findClassNameByClassId = async(id: number) =>{
-    const result = await Class.findOne({
-        where: {
-            id: id
-        }
-    })
-    if(result != null)
-        return result.clsName;
-    return null;
-}
-export const findEmailStudentByClassId = async(id: number) =>{
-    const strQuery = `select u."email" from "Users" as u, "Students" as s where s."userId" is not null
+export const findClassNameByClassId = async (id: number) => {
+	const result = await Class.findOne({
+		where: {
+			id: id
+		}
+	});
+	if (result != null) return result.clsName;
+	return null;
+};
+export const findEmailStudentByClassId = async (id: number) => {
+	const strQuery = `select u."email" from "Users" as u, "Students" as s where s."userId" is not null
     and s."classId" = ${id}
-    and u.id = s."userId" `
-    const result = await database.query(strQuery);
-    const emails: any = result[0];
+    and u.id = s."userId" `;
+	const result = await database.query(strQuery);
+	const emails: any = result[0];
 
-    const emailList = [];
-    for(var i = 0; i < emails.length; i++){
-        emailList[i] = emails[i].email;
-    }
-    return emailList;
+	const emailList = [];
+	for (var i = 0; i < emails.length; i++) {
+		emailList[i] = emails[i].email;
+	}
+	return emailList;
+};
 
-}
+export const deleteDataStudent = () => {
+	const strQuery = `delete from "Students"`;
+	database.query(strQuery);
+};
 
-export const deleteDataStudent = () =>{
-    const strQuery = `delete from "Students"`;
-    database.query(strQuery)
-}
+export const findStudentByStudentId = async (studentId: string) => {
+	const student = await Student.findOne({
+		where: {
+			studentId: studentId
+		}
+	});
 
-export const findStudentByStudentId = async(studentId: string)=>{
-    const student = await Student.findOne({
-        where: {
-            studentId: studentId
-        }
-    })
-
-    return student;
-
-}
+	return student;
+};
