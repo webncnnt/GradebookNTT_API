a423d6299232e14ac56702b0bf4509e4ee8174e4
huynhthinhi <huynhthinhi206@gmail.com>
2022-01-08 23:05:11 +0700

    refactor

--- src/components/review/review.model.ts
+++ src/components/review/review.model.ts
@@ -2,7 +2,7 @@ import { Review } from "@src/models/Review";
 import db from "@src/db";
 
 export const getAllOfReviews = async ()=>{
-    const strQuery = `select RV.id, RV."studentId", RV."statusTeacher", RV."statusStudent", 
+    const strQuery = `select RV.id as reviewId, RV."studentId", RV."statusTeacher", RV."statusStudent", 
     RV."expectedScore", RV."message", GA."title", GA."score" as scaleGrade 
     from "review" as RV, "gradeAssignments" as GA 
     where RV."assignmentId" = GA."id"`;
@@ -16,7 +16,7 @@ export const getAllOfReviews = async ()=>{
 
 export const gradeReviewDetail = async (reviewId: number) =>{
 
-    const strQuery = `select RV.id, RV."studentId", RV."statusTeacher", RV."statusStudent", 
+    const strQuery = `select RV.id as reviewId, RV."studentId", RV."statusTeacher", RV."statusStudent", 
     SG."score" as currentScore, RV."expectedScore", RV."message", GA."title", GA."score" as scaleGrade 
     from "review" as RV, "gradeAssignments" as GA, "StudentGrades" as SG 
     where RV.id = ${reviewId} and RV."assignmentId" = GA."id" and RV."studentId" = SG."studentId" 
